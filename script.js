f5237795d7ff5b234d6e940e90e31669448c9af1
Abhishek Waghmare <abhishekwaghmare1996@gmail.com>
2026-02-17 14:09:51 +0530

    Done some changes in SongList and in PlayBar

--- script.js
+++ script.js
@@ -1,1 +1,45 @@
 console.log("Lets write JavaScript");
+
+async function getSongs(){
+
+    let a = await fetch("http://127.0.0.1:3000/songs/");
+    let response = await a.text();
+    let div = document.createElement("div")
+    div.innerHTML = response;
+    let as = div.getElementsByTagName("a")
+    let songs = []
+    for (let index = 0; index < as.length; index++) {
+    const element = as[index];
+
+    // decode URL and replace backslashes
+    let href = decodeURIComponent(element.href).replaceAll("\\", "/");
+
+    if (href.includes("/songs/") && href.endsWith(".mp3")) {
+        let name = href.split("/songs/")[1];
+        songs.push(name);
+    }
+}
+
+    return songs;
+    // console.log(songs);
+}
+
+
+async function main(){
+    // Get the list of all songs
+    let songs = await getSongs()
+    console.log(songs);
+
+    let songUL = document.querySelector(".songList").getElementsByTagName("ul")[0]
+    for (const song of songs) {
+        songUL.innerHTML = songUL.innerHTML + `<li> ${song} </li>`
+    }
+    var audio = new Audio(songs[0]);
+    // audio.play(); 
+
+    audio.addEventListener("loadeddata",()=>{
+        console.log(audio.duration,audio.currentSrc,audio.currentTime);
+    })
+}
+ 
+main(); 
